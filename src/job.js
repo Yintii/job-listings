20b0b443bcd5106c2605519a4f0ea9783c955f13
yintii <keleheart1@gmail.com>
2020-07-03 21:51:06 -0600

    Almost finished the mobile site, need to implement the filiters

--- src/job.js
+++ src/job.js
@@ -5,31 +5,44 @@ function Job(props) {
   const requisites = props.requisites;
 
   const specialItems = specialDetails.map((specialDetail)=>
-    <li key={specialDetail.toString()} class="specialDetail">
-      {specialDetail}
-    </li>
+    <div key={specialDetail.toString()} className="specialDetail">
+      <div className="detail-text">
+        {specialDetail}
+      </div>
+    </div>
   );
 
   const theRequisites = requisites.map((requisite)=>
-  <li key={requisite.toString()} class="requisite">
-    {requisite}
-  </li>
+  <div key={requisite.toString()} className="requisite">
+    <div className="requisite-text">
+      {requisite}
+    </div>
+  </div>
   );
 
   return (
-    <div class="job-listing">
-    <div class="details">
-      <img src={props.img} />
-      <div class="employer">{props.employer}</div>
-      <div class="specialDetails">{specialItems}</div>
-      <div class="job-position">{props.position}</div>
-      <div class="listing-age">{props.age}</div>
-      <div class="job-time">{props.workTime}</div>
-      <div class="job-location">{props.jobLocation}</div>
+      <div className="job-listing">
+      {specialItems.length > 1 &&
+        <div className="stylishLine2"></div>
+      }
+        <img src={props.img} />
+        <div className="details">
+          <div className="listing-header">
+            <div className="employer">{props.employer}</div>
+            <div className="specialDetails">{specialItems}</div>
+          </div>
+          <div className="job-position">{props.position}</div>
+          <div className="sub-details">
+            <li className="listing-age">{props.age}</li>
+            <li className="job-time">{props.workTime}</li>
+            <li className="job-location">{props.jobLocation}</li>
+          </div>
+          <div className="stylishline"></div>
+        </div>
+        <div className="requirements">{theRequisites}</div>
     </div>
-    <div class="requirements">{theRequisites}</div>
-  </div>
-  );
+    );
+
 }
 
 export default Job;
